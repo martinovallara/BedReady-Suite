6609a8d60f52a607baeb4c1949bdb0408ac2103a
Martino Vallara <martino.vallara@gmail.com>
2023-11-23 18:46:41 +0100

    refactor: test

--- tests/use-case-booking.test.ts
+++ tests/use-case-booking.test.ts
@@ -1,21 +1,22 @@
 import { describe, expect, test } from '@jest/globals';
-
-
 import useCaseBeddingSetsStatusReport from '../src/use-case-bedding-sets-status-report';
 import { BeddingSetsStatusReport, Booking } from '../src/interfaces/bedding-sets-status-report';
+
 const date_zero = new Date(0);
 const day = 24 * 3600 * 1000;
+let report: BeddingSetsStatusReport ;
+const amountOfBeddingSet = 9;
+let beddingSetsStatusReport;
+
+beforeEach(() => {
+  beddingSetsStatusReport = useCaseBeddingSetsStatusReport();
+  beddingSetsStatusReport.addBeddingSets(amountOfBeddingSet);
+
+  report = beddingSetsStatusReport.report(date_zero, 5);
+})
 
 describe('beddingSetStatusReport', () => {
   it('should return all bedding sets cleaned at begin of process', () => {
-    const amountOfBeddingSet = 9;
-
-    const beddingSetsStatusReport = useCaseBeddingSetsStatusReport();
-
-    beddingSetsStatusReport.addBeddingSets(amountOfBeddingSet);
-
-    const report: BeddingSetsStatusReport = beddingSetsStatusReport.report(date_zero, 5);
-
     expect(report.days.length).toEqual(6);
 
     expect(report.days[0]).toEqual({ date: date_zero, cleaned: 9, in_use: 0, dirty: 0, in_laundery: 0 });
@@ -23,40 +24,64 @@ describe('beddingSetStatusReport', () => {
     expect(report.days[5]).toEqual({ date: new Date(5 * day), cleaned: 9, in_use: 0, dirty: 0, in_laundery: 0 });
   });
 
-
-
-
+  type BookingEvents = { sets: number }
   describe('when booking is made with ', () => {
-    const beddingSetsUsedTest = [2,4]
+    it.each([
+      { sets: 2 },
+      { sets: 4 }
+    ])('1 days and $sets bedding sets, should move it to in_use/dirty when check-in/check-out occur', ({sets}) => {
 
-    beddingSetsUsedTest.forEach(beddingSetsUsed => {
-      it(`should move ${beddingSetsUsed} bedding sets to in_use when check in occur`, () => {
-        const amountOfBeddingSet = 9;
-        const cleanedExpected = amountOfBeddingSet - beddingSetsUsed;
+      const cleanedExpected = amountOfBeddingSet - sets;
 
-        const beddingSetsStatusReport = useCaseBeddingSetsStatusReport();
-    
-        beddingSetsStatusReport.addBeddingSets(amountOfBeddingSet);
-    
-        const booking: Booking = {
-          checkInDate: new Date(1 * day),
-          checkOutDate: new Date(2 * day),
-          beddingSets: beddingSetsUsed
-        }
-    
-        beddingSetsStatusReport.bookingConfirmed(booking);
-    
-        const report: BeddingSetsStatusReport = beddingSetsStatusReport.report(date_zero, 5);
-    
-        expect(report.days.length).toEqual(6);
-    
-        expect(report.days[0]).toEqual({ date: date_zero, cleaned: amountOfBeddingSet, in_use: 0, dirty: 0, in_laundery: 0 });
+      const booking: Booking = {
+        checkInDate: new Date(1 * day),
+        checkOutDate: new Date(2 * day),
+        beddingSets: sets
+      }
 
-        expect(report.days[1]).toEqual({ date: new Date(1 * day), cleaned: cleanedExpected, in_use: beddingSetsUsed, dirty: 0, in_laundery: 0 });
-        expect(report.days[2]).toEqual({ date: new Date(2 * day), cleaned: cleanedExpected, in_use: 0, dirty: beddingSetsUsed, in_laundery: 0 });
-        expect(report.days[5]).toEqual({ date: new Date(5 * day), cleaned: cleanedExpected, in_use: 0, dirty: beddingSetsUsed, in_laundery: 0 });
-      });
-    })
-  })
-});
+      beddingSetsStatusReport.bookingConfirmed(booking);
+
+      const report: BeddingSetsStatusReport = beddingSetsStatusReport.report(date_zero, 5);
+
+      expect(report.days.length).toEqual(6);
+
+      expect(report.days[0]).toEqual({ date: date_zero, cleaned: amountOfBeddingSet, in_use: 0, dirty: 0, in_laundery: 0 });
+      expect(report.days[1]).toEqual({ date: new Date(1 * day), cleaned: cleanedExpected, in_use: sets, dirty: 0, in_laundery: 0 });
+      expect(report.days[2]).toEqual({ date: new Date(2 * day), cleaned: cleanedExpected, in_use: 0, dirty: sets, in_laundery: 0 });
+      expect(report.days[5]).toEqual({ date: new Date(5 * day), cleaned: cleanedExpected, in_use: 0, dirty: sets, in_laundery: 0 });
+    });
+
+
+    it.each([
+      { sets: 2 },
+      { sets: 4 }
+    ])('2 days and $sets bedding sets, should move it to in_use/dirty when check-in/check-out occur', ({sets}) => {
+      const amountOfBeddingSet = 9;
+      const cleanedExpected = amountOfBeddingSet - sets;
+
+      const beddingSetsStatusReport = useCaseBeddingSetsStatusReport();
+
+      beddingSetsStatusReport.addBeddingSets(amountOfBeddingSet);
+
+      const booking: Booking = {
+        checkInDate: new Date(1 * day),
+        checkOutDate: new Date(3 * day),
+        beddingSets: sets
+      }
+
+      beddingSetsStatusReport.bookingConfirmed(booking);
+
+      const report: BeddingSetsStatusReport = beddingSetsStatusReport.report(date_zero, 5);
+
+      expect(report.days.length).toEqual(6);
+
+      expect(report.days[0]).toEqual({ date: date_zero, cleaned: amountOfBeddingSet, in_use: 0, dirty: 0, in_laundery: 0 });
+      expect(report.days[1]).toEqual({ date: new Date(1 * day), cleaned: cleanedExpected, in_use: sets, dirty: 0, in_laundery: 0 });
+      expect(report.days[2]).toEqual({ date: new Date(2 * day), cleaned: cleanedExpected, in_use: sets, dirty: 0, in_laundery: 0 });
+      expect(report.days[3]).toEqual({ date: new Date(3 * day), cleaned: cleanedExpected, in_use: 0, dirty: sets, in_laundery: 0 });
+      expect(report.days[5]).toEqual({ date: new Date(5 * day), cleaned: cleanedExpected, in_use: 0, dirty: sets, in_laundery: 0 });
+    });
+  });
+})
+
 
