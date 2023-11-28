8afe8cd21ea3292a53850514d156c13c0eb7216c
Martino Vallara <martino.vallara@gmail.com>
2023-11-29 00:59:16 +0100

    fix input loop

--- src/promptLoop.ts
+++ src/promptLoop.ts
@@ -1,37 +1,54 @@
 import select from '@inquirer/select';
 import useCaseBookingInput from './app/console/use-case-booking/booking-handler.js';
 import useCaseInitBeddingSets from './app/console/use-case-init-bedding-sets/init-bedding-sets-handler.js';
+import { Separator } from '@inquirer/prompts';
 
 export async function promptLoop() {
-  const answer: 'initBeddingSets' | 'booking' = await select({
-    message: 'Seleziona il comando da eseguire',
-    choices: [
-      {
-        name: 'setup',
-        value: 'initBeddingSets',
-        description: 'Imposta il numero di sets matrimoniali in ogni stato (pulite, in uso, sporche, in pulizia, in lavanderia) ',
-      },
-      {
-        name: 'booking',
-        value: 'booking',
-        description: 'registra la prenotazione, con data check-in, set matrimoniali, e data di check-out.',
-      },
-    ]
-  });
 
-  console.log(answer);
+  type Command = 'initBeddingSets' | 'booking' | 'exit';
+  function askCommand(): Promise<Command> {
+    return select({
+      message: 'Seleziona il comando da eseguire',
+      choices: [
+        {
+          name: 'setup',
+          value: 'initBeddingSets',
+          description: 'Imposta il numero di sets matrimoniali in ogni stato (pulite, in uso, sporche, in pulizia, in lavanderia) ',
+        },
+        {
+          name: 'booking',
+          value: 'booking',
+          description: 'registra la prenotazione, con data check-in, set matrimoniali, e data di check-out.',
+        },
+        new Separator(),
+        {
+          name: 'exit',
+          value: 'exit',
+          description: 'esci',
+        }
+      ]
+    });
+  }
 
-  if(answer === 'initBeddingSets') {
-    await useCaseInitBeddingSets();
-  };
+ 
 
-  if (answer === 'booking') {
-    await useCaseBookingInput();
-  };
+  let answer = await askCommand();
 
+  while (answer !== 'exit') {
 
+    console.log(answer);
 
-  
+    if (answer === 'initBeddingSets') {
+      await useCaseInitBeddingSets();
+    };
+
+    if (answer === 'booking') {
+      await useCaseBookingInput();
+    };
+
+    answer = await askCommand();
+  } 
+
 }
 
 
