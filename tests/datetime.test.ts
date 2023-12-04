ead45beaf999058c66b08cbd37273b5eac16df4f
Martino Vallara <martino.vallara@gmail.com>
2023-12-04 22:36:53 +0100

    fix build

--- tests/datetime.test.ts
+++ tests/datetime.test.ts
@@ -5,7 +5,6 @@ describe('parseJSDate', () => {
     test('parser from string format dd/LL/yy', () => {
         const date = parseJSDate('31/12/23');
         expect(date).toStrictEqual(new Date(2023, 12-1, 31));
-        expect(date.toISOString()).toBe('2023-12-30T23:00:00.000Z');
    })
 
     test('parser from string format dd-LL-yy', () => {
